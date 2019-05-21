0e2a2eafd8768b651adcd5c29fb0af0b9ec7212f
LucasLimaAZ <40613140+LucasLimaAZ@users.noreply.github.com>
2019-05-21 19:50:12 -0300

    save

--- public/assets/js/questoes.js
+++ public/assets/js/questoes.js
@@ -1,13 +1,42 @@
 $(document).ready(function(){
 
     $('.hide').hide();
+    $('.CE').hide();
 
 });
 
-function rename(objeto){
+function alterar(objeto){
 
     var id = objeto.id;
 
-    $(`#editar-${id}`).toggle(300);
+    if($(`#modalidade-${id}`).val() == 'multipla-escolha'){
+        $('.ME').show(300);
+        $('.CE').hide(300);
+    }
+
+    if($(`#modalidade-${id}`).val() == 'certo ou errado'){
+        $('.ME').hide(300);
+        $('.CE').show(300);
+    }
+
+    $('#alterar-questao').html($(`#questao-${id}`).val());
+    $('#alterar-tags').html($(`#tags-${id}`).val());
+    $('#alterar-a').val($(`#a-${id}`).val());
+    $('#alterar-b').val($(`#b-${id}`).val());
+    $('#alterar-c').val($(`#c-${id}`).val());
+    $('#alterar-d').val($(`#d-${id}`).val());
+    $('#alterar-e').val($(`#e-${id}`).val());
+    $('#alterar-resposta').val($(`#resposta-${id}`).val());
+    $('#alterar-certoErrado').val($(`#certoErrado-${id}`).val());
+    $('#alterar-disciplina').val($(`#disciplina-${id}`).val());
+    $('#alterar-assunto').val($(`#assunto-${id}`).val());
+    $('#alterar-banca').val($(`#banca-${id}`).val());
+    $('#alterar-instituicao').val($(`#instituicao-${id}`).val());
+    $('#alterar-ano').val($(`#ano-${id}`).val());
+    $('#alterar-cargo').val($(`#cargo-${id}`).val());
+    $('#alterar-nivel').val($(`#nivel-${id}`).val());
+    $('#alterar-area_atuacao').val($(`#area_atuacao-${id}`).val());
+    $('#alterar-area_formacao').val($(`#area_formacao-${id}`).val());
+    $('#alterar-dificuldade').val($(`#dificuldade-${id}`).val());
 
 }
--- views/questoes.php
+++ views/questoes.php
@@ -28,78 +28,123 @@
                                             <i class="fa fa-trash"></i>
                                         </button>
                                     </form>
-                                    <button type="submit" name="questao" id="<?=$questao->id;?>" onclick="rename(this);" value="<?=$questao->id;?>" class="botao-editar" href="#">
+                                    <button type="submit" name="questao" id="<?=$questao->id;?>" onclick="alterar(this);" value="<?=$questao->id;?>" class="botao-editar" href="#">
                                         <i class="fa fa-edit"></i>
                                     </button>
                                 </div>
                             </div>
-                        </li>
-                        <li style="display:none"></li>
-                        <li class="hide" id="editar-<?=$questao->id;?>">
-                            <form method="POST" action="rename">
-                                <div class="row">
-                                    <div class="col-md-3">
-                                        <label for="questao">Questão: </label>
-                                        <textarea type="text" class="form-control" name="questao" placeholder="Questão..."><?=$questao->questao;?></textarea>
-                                    </div>
-                                    <div class="col-md-2">
-                                        <label for="tags">Tags: </label>
-                                        <textarea type="text" class="form-control" name="tags" placeholder="Tags..."><?=$questao->tags;?></textarea>
-                                    </div>
-                                    <div class="col-md-2">
-                                        <?php if($questao->modalidade == 'multipla-escolha'){ ?>
-                                        <label for="resposta">Resposta: </label>
-                                        <select name="resposta" class="form-control">
-                                            <option value="a">A</option>
-                                            <option value="b">B</option>
-                                            <option value="c">C</option>
-                                            <option value="d">D</option>
-                                            <option value="e">E</option>
-                                        </select>
-                                        <?php }else{ ?>
-                                        <label for="certoErrado">Resposta: </label>
-                                        <select name="certoErrado" class="form-control">
-                                            <option value="c">Certo</option>
-                                            <option value="e">Errado</option>
-                                        </select>
-                                        <?php } ?>
-                                    </div>
-                                    <div class="col-md-2">
-                                        <label for="asssunto">Assunto: </label>
-                                        <input type="text" class="form-control" name="assunto" value="<?=$questao->assunto;?>">
-                                    </div>
-                                    <div class="col-md-2">
-                                        <label for="banca">Banca: </label>
-                                        <input type="text" class="form-control" name="banca" value="<?=$questao->banca;?>">
-                                    </div>
-                                    <div class="col-md-2">
-                                        <label for="instituicao">Instituição: </label>
-                                        <input type="text" class="form-control" name="instituicao" value="<?=$questao->instituicao;?>">
-                                    </div>
-                                    <div class="col-md-2">
-                                        <label for="disciplina">Disciplina: </label>
-                                        <input type="text" class="form-control" name="disciplina" value="<?=$questao->disciplina;?>">
-                                    </div>
-                                    <div class="col-md-2">
-                                        <label for="ano">Ano: </label>
-                                        <input type="text" class="form-control" name="ano" value="<?=$questao->ano;?>">
-                                    </div>
-                                    <div class="col-md-2">
-                                        <label for="cargo">Cargo: </label>
-                                        <input type="text" class="form-control" name="cargo" value="<?=$questao->cargo;?>">
-                                    </div>
-                                    <div class="col-md-1">
-                                        <button type="submit" name="questao" value="<?=$questao->id;?>" class="botaoQuestao" href="#">
-                                            Salvar
-                                        </button>
-                                    </div>                                                                                                                                   
-                                </div>
-                            </form>
+                            <input type="hidden" value="<?=$questao->id;?>" id="<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->questao;?>" id="questao-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->tags;?>" id="tags-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->a;?>" id="a-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->b;?>" id="b-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->c;?>" id="c-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->d;?>" id="d-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->e;?>" id="e-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->resposta;?>" id="resposta-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->certoErrado;?>" id="certoErrado-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->disciplina;?>" id="disciplina-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->assunto;?>" id="assunto-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->banca;?>" id="banca-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->instituicao;?>" id="instituicao-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->ano;?>" id="ano-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->cargo;?>" id="cargo-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->nivel;?>" id="nivel-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->area_formacao;?>" id="area_formacao-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->area_atuacao;?>" id="area_atuacao-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->modalidade;?>" id="modalidade-<?=$questao->id;?>">
+                            <input type="hidden" value="<?=$questao->dificuldade;?>" id="dificuldade-<?=$questao->id;?>">
                         </li>
                         <?php
                         endforeach; 
                         ?>
                         </ul>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="row usersBox">
+                            <div class="col-md-12">
+                                <h2 style="text-align:center;padding:1%"><i class="fa fa-edit"></i> Editar Questão</h2>
+                            </div>
+                            <div class="col-md-8">
+                                <label for="alterar-questao">Questão: </label>
+                                <textarea class="form-control" placeholder="Questão..." id="alterar-questao" name="questao"></textarea>
+                            </div>
+                            <div class="col-md-4">
+                                <label for="alterar-tags"><i class="fa fa-tags"></i> Tags: </label>
+                                <textarea class="form-control" placeholder="Tags..." id="alterar-tags" name="tags"></textarea>
+                            </div>
+                            <div class="paddingTop2 col-md-6">
+                                <label for="alterar-a">Alternativa <b>A</b>: </label>
+                                <input type="text" class="form-control" id="alterar-a" name="a">
+                            </div>
+                            <div class="paddingTop2 col-md-6 ME">
+                                <label for="alterar-b">Alternativa <b>B</b>: </label>
+                                <input type="text" class="form-control" id="alterar-b" name="b">
+                            </div>
+                            <div class="paddingTop2 col-md-6 ME">
+                                <label for="alterar-c">Alternativa <b>C</b>: </label>
+                                <input type="text" class="form-control" id="alterar-c" name="c">
+                            </div>
+                            <div class="paddingTop2 col-md-6 ME">
+                                <label for="alterar-d">Alternativa <b>D</b>: </label>
+                                <input type="text" class="form-control" id="alterar-d" name="d">
+                            </div>
+                            <div class="paddingTop2 col-md-6 ME">
+                                <label for="alterar-e">Alternativa <b>E</b>: </label>
+                                <input type="text" class="form-control" id="alterar-e" name="e">
+                            </div>
+                            <div class="paddingTop2 col-md-6 ME">
+                                <label for="alterar-resposta">Resposta: </label>
+                                <input type="text" class="form-control" id="alterar-resposta" name="resposta">
+                            </div>
+                            <div class="paddingTop2 col-md-6 CE">
+                                <label class="CE" for="alterar-certoErrado">Resposta: </label>
+                                <input type="text" class="form-control CE" id="alterar-certoErrado" name="certoErrado">
+                            </div>
+                            <div class="paddingTop2 col-md-4">
+                                <label for="alterar-disciplina"><i class="fa fa-book"></i> Disciplina: </label>
+                                <input type="text" class="form-control" id="alterar-disciplina" name="disciplina">
+                            </div>
+                            <div class="paddingTop2 col-md-4">
+                                <label for="alterar-assunto"><i class="fa fa-comment"></i> Assunto: </label>
+                                <input type="text" class="form-control" id="alterar-assunto" name="assunto">
+                            </div>
+                            <div class="paddingTop2 col-md-4">
+                                <label for="alterar-banca"><i class="fa fa-table"></i> Banca: </label>
+                                <input type="text" class="form-control" id="alterar-banca" name="banca">
+                            </div>
+                            <div class="paddingTop2 col-md-4">
+                                <label for="alterar-instituicao"><i class="fa fa-home"></i> Instituição: </label>
+                                <input type="text" class="form-control" id="alterar-instituicao" name="instituicao">
+                            </div>
+                            <div class="paddingTop2 col-md-4">
+                                <label for="alterar-ano"><i class="fa fa-calendar"></i> Ano: </label>
+                                <input type="text" class="form-control" id="alterar-ano" name="ano">
+                            </div>
+                            <div class="paddingTop2 col-md-4">
+                                <label for="alterar-cargo"><i class="fa fa-user"></i> Cargo: </label>
+                                <input type="text" class="form-control" id="alterar-cargo" name="cargo">
+                            </div>
+                            <div class="paddingTop2 col-md-4">
+                                <label for="alterar-nivel"><i class="fa fa-arrow-up"></i> Nível: </label>
+                                <input type="text" class="form-control" id="alterar-nivel" name="nivel">
+                            </div>
+                            <div class="paddingTop2 col-md-4">
+                                <label for="alterar-area_atuacao"><i class="fa fa-briefcase"></i> Área de Atuação: </label>
+                                <input type="text" class="form-control" id="alterar-area_atuacao" name="area_atuacao">
+                            </div>
+                            <div class="paddingTop2 col-md-4">
+                                <label for="alterar-area_formacao"><i class="fa fa-feather"></i> Área de Formação: </label>
+                                <input type="text" class="form-control" id="alterar-area_formacao" name="area_formacao">
+                            </div>
+                            <div class="paddingTop2 col-md-4">
+                                <label for="alterar-dificuldade"><i class="fa fa-arrow-down"></i> Dificuldade: </label>
+                                <input type="text" class="form-control" id="alterar-dificuldade" name="dificuldade">
+                            </div>
+                            <div style="padding-bottom:2%;" class="paddingTop2 col-md-12">
+                                <button style="background-color:#D2AE6D;color:white;" type="submit" class="form-control col-md-4 offset-md-4">Atualizar</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
